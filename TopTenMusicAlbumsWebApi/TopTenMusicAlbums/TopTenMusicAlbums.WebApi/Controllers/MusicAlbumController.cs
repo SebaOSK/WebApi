f762194a5688c7e3b63d4f0d73d327f9f5324a4c
Sebastian Tkalčić <stkalcic00@gmail.com>
2023-05-25 23:19:45 +0200

    added the DELETE method

--- TopTenMusicAlbumsWebApi/TopTenMusicAlbums/TopTenMusicAlbums.WebApi/Controllers/MusicAlbumController.cs
+++ TopTenMusicAlbumsWebApi/TopTenMusicAlbums/TopTenMusicAlbums.WebApi/Controllers/MusicAlbumController.cs
@@ -44,18 +44,28 @@ namespace TopTenMusicAlbums.WebApi.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody] string value)
+        public string Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
+        public string Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public List<MusicAlbum> Delete(int id)
         {
+            /* foreach(MusicAlbum album in listOfAlbum)
+             {
+                 if (album.Id == id)
+                 {
+                     listOfAlbum.Remove(album);
+                     break;
+                 };
+             }*/
+            listOfAlbum.RemoveAll(p => p.Id == id);
+            return listOfAlbum;
         }
     }
 }
